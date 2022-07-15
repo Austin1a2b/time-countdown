67a52c27300eb6f28b282bf5a8b00aabb4e7ec9a
Austin1a2b <cavalier283@gmail.com>
2022-07-15 23:24:32 +0800

    成功分離task-list

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,5 +1,195 @@
 <template>
+  <!-- 清單表頭 -->
+  <div class="tasks-wrapper mt-4">
+    <div class="list-head d-flex justify-content-between">
+      <div class="task-head">待辦事項列表</div>
+      <div class="dropdown">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="manage-task"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          管理列表
+        </button>
+        <div class="dropdown-menu" aria-labelledby="manage-task">
+          <button
+            @click.stop.prevent="deleteTask('allTask')"
+            class="dropdown-item"
+          >
+            刪除所有待辦
+          </button>
+          <button
+            @click.stop.prevent="clearExecutedTimes"
+            class="dropdown-item"
+          >
+            清除所有記數
+          </button>
+          <button class="dropdown-item"></button>
+        </div>
+      </div>
+    </div>
 
+    <!-- 列表 標題&設定 容器div  -->
+    <div
+      class="task-list mt-2"
+      @click="transferExecuteId(task.id)"
+      v-for="task in tasks"
+      :key="task.id"
+      :class="{ isexecute: executeTaskId === task.id }"
+    >
+      <div v-if="task.id !== clickId">
+        <div class="d-flex justify-content-between">
+          <div>
+            <input type="checkbox" v-model="task.complete" @click.stop="" />
+            <span :class="{ iscomplete: task.complete }">{{ task.title }}</span>
+          </div>
+          <div>
+            <span class="mr-2" :class="{ iscomplete: task.complete }"
+              >{{ task.executedTimes }}/{{ task.estimatedTimes }}
+            </span>
+            <span for="task-setting" @click.stop="toggleSetting(task.id)"
+              >設定</span
+            >
+          </div>
+        </div>
+        <div class="mx-3 text-wrap">
+          <p :class="{ iscomplete: task.complete }">{{ task.note }}</p>
+        </div>
+      </div>
+      <!-- 設定內容 -->
+      <form
+        id="edit-task"
+        v-if="task.id === clickId"
+        @submit.stop.prevent="editTask(task.id)"
+        @click.stop=""
+      >
+        <!-- title -->
+        <span> 待辦事項:</span>
+        <input type="text" :value="task.title" name="title" />
+        <!-- 計次 -->
+        <div>
+          <p>已使用/預計需要 幾個番茄時鐘</p>
+          <input
+            :value="task.executedTimes"
+            type="number"
+            name="executedTimes"
+          />
+          <span> / </span>
+          <input
+            :value="task.estimatedTimes"
+            type="number"
+            min="1"
+            name="estimatedTimes"
+          />
+        </div>
+        <div>
+          <div>註解</div>
+          <textarea :value="task.note" type="text" name="note" />
+        </div>
+        <div class="d-flex justify-content-around">
+          <button @click.stop="deleteTask(task.id)">刪除</button>
+          <button class="mr-4" @click.stop="toggleSetting('close')">
+            取消
+          </button>
+          <button type="submit" class="mr-2 btn btn-primary">確認</button>
+        </div>
+      </form>
+    </div>
+    <!-- 以上為列表 -->
 
-
+    <div>
+      <div @click="toggleSetting('new')">新增</div>
+    </div>
+    <!-- 新增 -->
+    <form
+      id="create-task"
+      @submit.stop.prevent="createTask"
+      v-if="clickId === 'new'"
+    >
+      <span> 待辦事項:</span>
+      <input type="text" name="title" />
+      <div>
+        <p>預計需要 幾個番茄時鐘</p>
+        <input type="number" name="estimatedTimes" min="1" />
+      </div>
+      <div>
+        <div>註解</div>
+        <textarea type="text" name="note" />
+      </div>
+      <div class="d-flex justify-content-around">
+        <button class="mr-4" @click="toggleSetting('close')">取消</button>
+        <button type="submit" class="mr-2 btn btn-primary">確認</button>
+      </div>
+    </form>
+    <!-- 新增-->
+  </div>
 </template>
+
+
+<script>
+export default {
+  props: {
+    tasks: {
+      type: Array,
+      default: -1,
+    },
+    executeTaskId: {
+      type: String,
+      default: -1,
+    },
+  },
+  data() {
+    return {
+      clickId: "close",
+    };
+  },
+  methods: {
+    deleteTask(taskId) {
+      this.$emit("after-deleteTask", taskId);
+    },
+    toggleSetting(taskId) {
+      this.clickId = taskId;
+    },
+    createTask(e) {
+      const form = e.target;
+      const formData = new FormData(form);
+      this.$emit("after-create-task", Object.fromEntries(formData));
+      this.toggleSetting("close");
+    },
+    editTask(id) {
+      const form = document.getElementById("edit-task");
+      const formData = new FormData(form);
+      let editData = Object.fromEntries(formData);
+      editData = {
+        ...editData,
+        id,
+      };
+      this.$emit("after-edit-task", editData);
+      this.toggleSetting("close");
+    },
+    transferExecuteId(id) {
+      console.log(id);
+      console.log(this.executeTaskId);
+      this.$emit("change-execute-task", id);
+    },
+    clearExecutedTimes(){
+      
+    }
+  },
+};
+</script>
+
+<style>
+.iscomplete {
+  text-decoration: line-through;
+}
+.task-list {
+  border: 1px solid black;
+}
+.isexecute {
+  background-color: white;
+}
+</style>
--- src/views/TimeCountdown.vue
+++ src/views/TimeCountdown.vue
@@ -1,27 +1,25 @@
 <template>
   <div class="body">
     <div class="main">
-      <div class="d-flex justify-content-around change-time-countdown">
+      <div class="pt-5 d-flex justify-content-around change-time-countdown">
         <button
           type="button"
           class="btn btn-outline-info"
-          @click="chooseCountdown(countdownSetting.focusTime, 'focus')"
+          @click="chooseCountdown('focus')"
         >
           專注時間
         </button>
         <button
           type="button"
           class="btn btn-outline-info"
-          @click="
-            chooseCountdown(countdownSetting.shortBreakTime, 'shortBreak')
-          "
+          @click="chooseCountdown('shortBreak')"
         >
           小休息時間
         </button>
         <button
           type="button"
           class="btn btn-outline-info"
-          @click="chooseCountdown(countdownSetting.longBreakTime, 'longBreak')"
+          @click="chooseCountdown('longBreak')"
         >
           長休息時間
         </button>
@@ -47,99 +45,15 @@
       </div>
 
       <div>專注次數 {{ times }}</div>
-
-      <!-- 清單表頭 -->
-      <div class="tasks-wrapper mt-4">
-        <div class="list-head d-flex justify-content-between">
-          <div class="task-head">待辦事項列表</div>
-          <div class="dropdown">
-            <button
-              class="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="manage-task"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              管理列表
-            </button>
-            <div class="dropdown-menu" aria-labelledby="manage-task">
-              <button @click="deleteTask('allTask')" class="dropdown-item">
-                刪除所有待辦
-              </button>
-              <button class="dropdown-item">清除所有記數</button>
-              <button class="dropdown-item"></button>
-            </div>
-          </div>
-        </div>
-
-        <div class="task-list mt-2" v-for="task in tasks" :key="task.id">
-          <!-- 列表 最外層標題  -->
-          <div class="d-flex justify-content-between">
-            <div>
-              <input type="checkbox" name="" />
-              <span>{{ task.title }}</span>
-            </div>
-            <div>
-              <span>後續要補上數字? / ? </span>
-              <span for="task-setting" @click="toggleSetting(task.id)"
-                >設定</span
-              >
-            </div>
-          </div>
-          <!-- 設定內容 -->
-          <form class="edit-task" v-if="task.id === clickId">
-            <!-- title -->
-            <span> 待辦事項:</span>
-            <input type="text" v-model="task.title" />
-            <!-- 計次 -->
-            <div>
-              <p>已使用/預計需要 幾個番茄時鐘</p>
-              <input type="number" v-model="times" />
-              <span> / </span>
-              <input type="number" min="1" />
-            </div>
-            <!-- note -->
-            <div>
-              <div>註解</div>
-              <textarea type="text" name="note" id="" />
-            </div>
-            <!-- 按鈕 -->
-            <div class="d-flex justify-content-around">
-              <button @click="deleteTask(task.id)">刪除</button>
-              <button class="mr-4" @click="toggleSetting('close')">取消</button>
-              <button type="submit" class="mr-2 btn btn-primary">確認</button>
-            </div>
-          </form>
-        </div>
-        <div>
-          <div @click="toggleSetting('new')">新增</div>
-        </div>
-        <!-- 新增 -->
-        <!-- 設定內容 -->
-        <form class="create-task" v-if="clickId === 'new'">
-          <!-- title -->
-          <span> 待辦事項:</span>
-          <input type="text" />
-          <!-- 計次 -->
-          <div>
-            <p>預計需要 幾個番茄時鐘</p>
-            <input type="number" min="1" />
-          </div>
-          <!-- note -->
-          <div>
-            <div>註解</div>
-            <textarea type="text" name="note" id="" />
-          </div>
-          <!-- 按鈕 -->
-          <div class="d-flex justify-content-around">
-            <button>刪除</button>
-            <button class="mr-4" @click="toggleSetting('close')">取消</button>
-            <button type="submit" class="mr-2 btn btn-primary">確認</button>
-          </div>
-        </form>
-        <!-- 新增-->
-      </div>
+      
+      <TaskList
+        :tasks="tasks"
+        :executeTaskId="executeTaskId"
+        @after-deleteTask="afterDeleteTask"
+        @after-create-task="afterCreateTask"
+        @after-edit-task="afterEditTask"
+        @change-execute-task="changeExecuteTask"
+      />
     </div>
   </div>
 </template>
@@ -147,38 +61,40 @@
 
 <script>
 import TimeSettingModal from "../components/TimeSettingModal.vue";
-// import { v4 as uuidv4 } from "uuid";
+import TaskList from "../components/TaskList.vue";
+import { v4 as uuidv4 } from "uuid";
 
 export default {
   components: {
     TimeSettingModal,
+    TaskList,
   },
   data() {
     return {
       tasks: [
         {
           title: "待辦事項1",
-          id: 1,
-          complete: false,
-          note: "",
-          estimatedTimes: 1,
-          executedTimes: 0,
+          id: "1",
+          complete: true,
+          estimatedTimes: 3,
+          executedTimes: 1,
+          note: "內容長一點 測試換行 與版面-----內容長一點 測試換行 與版面內容長一點 測試換行 與版面內容長一點 測試換行 與版面內容長一點 測試換行 與版面內容長一點 測試換行 與版面內容長一點 測試換行 與版面內容長一點 測試換行 與版面",
         },
         {
           title: "待辦事項222",
-          id: 2,
+          id: "2",
           complete: false,
-          note: "",
-          estimatedTimes: 1,
-          executedTimes: 0,
+          estimatedTimes: 4,
+          executedTimes: 2,
+          note: "這邊是 註解 2",
         },
         {
           title: "待辦事項3333",
-          id: 3,
+          id: "3",
           complete: false,
-          note: "",
-          estimatedTimes: 1,
-          executedTimes: 0,
+          estimatedTimes: 5,
+          executedTimes: 2,
+          note: "這邊是 註解 3",
         },
       ],
       countdownState: "pause",
@@ -192,14 +108,23 @@ export default {
       times: 1,
       countdownMode: "focus",
       setIntervalId: -1,
-      clickId: "close",
+      executeTaskId: "-1",
     };
   },
   methods: {
-    chooseCountdown(time, mode) {
-      this.countdownMinutes = time;
-      this.countdownSeconds = 0;
+    chooseCountdown(mode) {
+      if (this.countdownState === "start") {
+        console.log("要設計提醒視窗");
+      }
       this.countdownMode = mode;
+      this.countdownSeconds = 0;
+      if (mode === "focus") {
+        this.countdownMinutes = this.countdownSetting.focusTime;
+      } else if (mode === "shortBreak") {
+        this.countdownMinutes = this.countdownSetting.shortBreakTime;
+      } else {
+        this.countdownMinutes = this.countdownSetting.longBreakTime;
+      }
     },
     afterSaveSetting(setting) {
       this.countdownSetting = { ...setting };
@@ -217,7 +142,7 @@ export default {
           this.countdownState = "pause";
           this.timeIsUp();
         }
-      }, 1000);
+      }, 100);
     },
     pauseCountdown(id) {
       clearInterval(id);
@@ -226,41 +151,83 @@ export default {
     timeIsUp() {
       if (this.countdownMode === "focus" && this.times % 4 !== 0) {
         this.times++;
-        this.chooseCountdown(
-          this.countdownSetting.shortBreakTime,
-          "shortBreak"
-        );
-      } else if (this.countdownMode === "shortBreak") {
-        this.chooseCountdown(this.countdownSetting.focusTime, "focus");
+        this.chooseCountdown("shortBreak");
+        this.addexecutedTimes(this.executeTaskId);
       } else if (this.countdownMode === "focus") {
         this.times++;
-        this.chooseCountdown(this.countdownSetting.longBreakTime, "longBreak");
+        this.chooseCountdown("longBreak");
+        this.addexecutedTimes(this.executeTaskId);
+      } else if (this.countdownMode === "shortBreak") {
+        this.chooseCountdown("focus");
       } else if (this.countdownMode === "longBreak") {
-        this.chooseCountdown(this.countdownSetting.focusTime, "focus");
+        this.chooseCountdown("focus");
       }
     },
-    toggleSetting(taskId) {
-      this.clickId = taskId;
-    },
-    deleteTask(taskId) {
+    afterDeleteTask(taskId) {
       if (taskId === "allTask") {
         this.tasks = [];
       } else {
-        console.log("in");
         this.tasks = this.tasks.filter((task) => {
           return task.id !== taskId;
         });
       }
     },
+    afterCreateTask(task) {
+      task = {
+        ...task,
+        id: uuidv4(),
+        executedTimes: 0,
+        complete: false,
+      };
+      this.tasks.push(task);
+    },
+    afterEditTask(editData) {
+      this.tasks = this.tasks.map((task) => {
+        if (editData.id === task.id) {
+          task = {
+            ...task,
+            ...editData,
+          };
+          return task;
+        } else {
+          return task;
+        }
+      });
+    },
+    changeExecuteTask(taskId) {
+      this.executeTaskId = taskId;
+    },
+    addexecutedTimes(taskId) {
+      this.tasks = this.tasks.map((task) => {
+        if (taskId === task.id) {
+          task = {
+            ...task,
+            executedTimes: task.executedTimes + 1,
+          };
+          return task;
+        } else {
+          return task;
+        }
+      });
+    },
   },
-  // 原本 預計 用 filters 功能 讓 秒數=0 ; 回傳 00
-  // 後續想到 可以在 倒數計時的功能裡面 達成這項功能
   filters: {
     changeNumberDisplay(n) {
-      if (n === 0) {
-        return "00";
+      if (n < 10) {
+        return "0" + n;
       } else return n;
     },
+  },
+  watch: {
+    countdownSetting: {
+      handler: function () {
+        this.chooseCountdown(this.countdownMode);
+      },
+      deep: true,
+    },
+  },
+  created() {
+    this.executeTaskId = this.tasks[0].id;
   },
 };
 </script>
@@ -283,10 +250,4 @@ export default {
   font-weight: bold;
   text-align: center;
 }
-
-.task-list {
-  border: 1px solid black;
-  margin-top: 2px;
-}
 </style>
-
